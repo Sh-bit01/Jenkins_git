eac68290d663fb6c51bd06e860fe4dd142d58d93
ubuntu_server <ubuntu@server.com>
2025-05-05 15:59:58 +0530

    docker error

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "Hello from Jenkins Pipeline!"
+    return 'Hello from Flask in Docker!'
 
-if __name__ == "__main__":
-    app.run(port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
